4d1eb65d83e6309803cc7e6a67078f46337783a1
Ankit Jaiswal <jankit360@gmail.com>
2021-05-29 02:02:11 +0530

    DBdesign: done

--- AmazeSafe/AppHome/models.py
+++ AmazeSafe/AppHome/models.py
@@ -3,15 +3,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserInfo(models.Model):
-    userId =  models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
+    userInstance =  models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
     userMode = models.CharField(max_length=1) 
+    adafruitToken = models.CharField(max_length = 1000,default = "")
+    adafruitUserName = models.CharField(max_length = 500,default = "")
 
     def __str__(self):
         return self.name+" "+str(self.user_id)
 
 
 class AmazeUsersOrders(models.Models):
-    userId = models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
+    userInstance = models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
     orderName = models.CharField(max_length = 500)
     orderAddress = models.CharField(max_length = 1000)
     orderCost = models.FloatField()
@@ -24,6 +26,6 @@ class AmazeUsersOrders(models.Models):
 
 
 class AmazeWarriorsOrders(models.Model):
-    userId = models.ForeignKey( User, null = True, on_delete = models.SET_NULL)
+    userInstance = models.ForeignKey( User, null = True, on_delete = models.SET_NULL)
     orderId = models.ForeignKey(AmazeUsersOrders, null = True, on_delete = models.SET_NULL)
     
